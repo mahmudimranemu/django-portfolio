ce1e76066e1575c57ad814ae6bdf62e13c4db2d2
mahmudimranemu <mahmudimranemu@gmail.com>
2024-02-06 17:00:55 +0600

    First commit

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('accounts/', include("django.contrib.auth.urls")),
+    path('dashboard/', views.dashboard, name="dashboard"),
+    path('oauth/', include("social_django.urls")),
+    path('register/', views.register, name="register")
+]
+
